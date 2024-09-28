38f54b2441af31324b8e1fd5ffd5b10d6cf90879
Christopher Pinedo <christopher.pinedo@hotmail.com>
2024-09-28 10:54:49 -0500

    Agregando EP01 pendientes

---
+++ 2024-02/1072-AlgoritmoYEstructuraDeDatos/Laboratorios/Lab01/Project1/src/main/java/cdpg/project1/newpackage/Ejemplo2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cdpg.project1.newpackage;
+
+/**
+ *
+ * @author chris
+ */
+public class Ejemplo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Curso AED");
+    }
+    
+}
---
+++ 2024-02/1072-AlgoritmoYEstructuraDeDatos/Laboratorios/Lab01/Project1/src/main/java/cdpg/project1/newpackage/NewMain.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cdpg.project1.newpackage;
+
+/**
+ *
+ * @author chris
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Verano");
+        System.out.println("Otoño");
+        System.out.println("Invierno");
+        System.out.println("Primavera");
+    }
+    
+}
