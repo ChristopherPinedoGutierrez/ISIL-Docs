01174c91536fd29a702f8be2b8b65be0b25238fb
Christopher Pinedo <christopher.pinedo@hotmail.com>
2024-10-24 09:38:01 -0500

    Parcial - Gestion de la creatividad

---
+++ 2024-02/3079-ProgramacionAvanzadaDeBasesDeDatos/Evaluaciones/PARCIAL/Preguntas-ExamenParcial.sql
@@ -0,0 +1,56 @@
+USE SumaQPizza;
+
+/* PREGUNTA 1 (3 puntos)
+Independientemente de la medida de la pizza, ¿cuál es el sabor de pizza
+que tiene más demanda entre los clientes de SumaQPizza?
+*/
+
+-- Digite su respuesta aquí
+
+
+
+/* PREGUNTA 2 (3 puntos) 
+Para cada sabor de pizza, ¿cuál es el monto total de dinero
+ingresado en el mes de diciembre? El monto total por cada sabor de 
+pizza se debe mostrar desagregado por la medida de la pizza,
+y ordenado de mayor a menor monto total.
+*/
+
+-- Digite su respuesta aquí
+
+
+
+/* PREGUNTA 3 (4 puntos)
+Para las pizzas vegetarianas (categoría Veggie), ¿cuántas unidades 
+mensuales por sabor de pizza se han vendido en el año para las pizzas
+que tienen como ingrediente aceitunas (olives)?
+*/
+
+-- Digite su respuesta aquí
+
+
+
+/* PREGUNTA 4 (5 puntos)
+Para las ventas efectuadas en horas de la noche, ¿cuántas
+unidades en total se vendieron de 19:00 a 19:59 horas, de
+20:00 a 20:59, de 21:00 a 21:59, de 22:00 a 22:59, y de
+23:00 a 23:59 horas en la fecha 31 de diciembre?
+*/
+
+-- Digite su respuesta aquí
+
+
+
+/* PREGUNTA 5 (5 puntos)
+El supervisor de ventas, que es pariente del dueño de la empresa, 
+tiene la impresión de que en los meses octubre, noviembre y diciembre las 
+ventas en el horario de 23:00 a 23:59 horas son muy bajas, por lo que 
+está pensando sugerir a la Gerencia que el horario de atención sea solo 
+hasta las 22:59 y no hasta las 23:59 como es actualmente. 
+Elabore un informe que le permita al supervisor tomar la decisión de 
+hacer o no dicha sugerencia a la Gerencia.
+*/
+
+-- Digite su respuesta aquí
+
+
